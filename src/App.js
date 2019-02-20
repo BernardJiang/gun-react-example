0eb7e13439d30374869b9433b472db750d646430
bernardkerun <bernard.jiang@thehovercam.com>
2019-02-19 17:32:31 -0800

    Copied react app from gun. It avoids memory crash issue.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import Gun from 'gun/gun'
+import Todos from './Todos'
+import Chat from './Chat'
+import Json from './Json'
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    // console.log("dbg", 'origin=' + window.location.origin + ". port=" + window.location.port);
+
+    var newloc = window.location.origin;
+    if (window.location.port !== '8765') {
+      newloc = window.location.protocol + "//" 
+        + window.location.hostname 
+        + (window.location.port ? ':' + '8765' : '');
+    }
+
+    // console.log("dbg", 'origin=' + newloc + ". port=" + window.location.port);
+    this.gun = Gun(newloc + '/gun')
+    // this.gun = Gun('http://localhost:8765' + '/gun')
+
+  }
   render() {
     return (
       <div className="App">
@@ -20,6 +41,16 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <h2>Todo</h2>
+        <Todos gun={this.gun} />
+        <br />
+        <hr />
+        <h2>Chat</h2>
+        <Chat gun={this.gun} />
+        <br />
+        <hr />
+        <h2>Json</h2>
+        <Json gun={this.gun} />
       </div>
     );
   }
