e71377bce2266e861117e2797f98b20a57c6a5df
Bernard Jiang <hongyujiang@hotmail.com>
2019-04-09 21:24:38 -0700

    Don't process messages before gun kicks in.

--- src/lib/ChatBot.jsx
+++ src/lib/ChatBot.jsx
@@ -76,6 +76,10 @@ class ChatBot extends Component {
     //assume it has msgs.
     if(obj.msgs){
       const msgs = formatMsgs(obj.msgs)
+      msgs.forEach(a => {
+        console.log("Bernard", "a.key=" + a.key + ". msg=" + a.message)
+      })
+
       this.processSteps(msgs);  
     }
 
@@ -179,97 +183,6 @@ class ChatBot extends Component {
 
     this.entity && this.entity.onChatBotMessage(this.updateUIChatBot)
 
-    const { steps } = this.props;
-    const {
-      botDelay,
-      botAvatar,
-      cache,
-      cacheName,
-      customDelay,
-      enableMobileAutoFocus,
-      userAvatar,
-      userDelay
-    } = this.props;
-    const chatSteps = {};
-
-    const defaultBotSettings = { delay: botDelay, avatar: botAvatar };
-    const defaultUserSettings = { delay: userDelay, avatar: userAvatar, hideInput: false };
-    const defaultCustomSettings = { delay: customDelay };
-
-    for (let i = 0, len = steps.length; i < len; i += 1) {
-      const step = steps[i];
-      let settings = {};
-
-      if (step.user) {
-        settings = defaultUserSettings;
-      } else if (step.message || step.asMessage) {
-        settings = defaultBotSettings;
-      } else if (step.component) {
-        settings = defaultCustomSettings;
-      }
-
-      chatSteps[step.id] = Object.assign({}, settings, schema.parse(step));
-    }
-
-    schema.checkInvalidIds(chatSteps);
-
-    const firstStep = {}
-
-    if(steps.length>0){
-      firstStep = steps[0];
-
-      if (firstStep.message) {
-        const { message } = firstStep;
-        firstStep.message = typeof message === 'function' ? message() : message;
-        chatSteps[firstStep.id].message = firstStep.message;
-      }
-  
-    }
-
-    const { recognitionEnable } = this.state;
-    const { recognitionLang } = this.props;
-
-    if (recognitionEnable) {
-      this.recognition = new Recognition(
-        this.onRecognitionChange,
-        this.onRecognitionEnd,
-        this.onRecognitionStop,
-        recognitionLang
-      );
-    }
-
-    if (this.content) {
-      this.content.addEventListener('DOMNodeInserted', this.onNodeInserted);
-      window.addEventListener('resize', this.onResize);
-    }
-
-    const { currentStep, previousStep, previousSteps, renderedSteps } = storage.getData(
-      {
-        cacheName,
-        cache,
-        firstStep,
-        steps: chatSteps
-      },
-      () => {
-        // focus input if last step cached is a user step
-        this.setState({ disabled: false }, () => {
-          if (enableMobileAutoFocus || !isMobile()) {
-            if (this.input) {
-              this.input.focus();
-            }
-          }
-        });
-      }
-    );
-
-    this.setState({
-      currentStep,
-      defaultUserSettings,
-      previousStep,
-      previousSteps,
-      renderedSteps,
-      steps: chatSteps
-    });
   }
 
 
