a90a8db2447239c9aab391e09552b4c767ecc5c7
Bernard Jiang <hongyujiang@hotmail.com>
2020-11-08 22:05:06 -0800

    Added greeterComponent.

--- src/greeter2View.js
+++ src/greeter2View.js
@@ -9,4 +9,41 @@ export function greeter2View(name) {
       input({ sel: 'greeter2namewwww', type: 'text' })
     ])
   }
+
+export function greeterComponent(sources) {
+    const domSource = sources.react;
+    const props$ = sources.props;
+
+    const input$ = domSource
+      .select('greeterComponent')
+      .events('input')
+      .map(ev => { 
+        console.log(" greeter input ev=", ev);
+        return ev.target.value
+      });
+
+    const state$ = props$
+        .map( props => $input$
+            .map( val => ({
+                label: props.label,
+                value: val
+            })) 
+            .startWith(props)
+            )
+        .flatten()
+        .remember();  
+
+    const vdom$ = state$
+       .map( state => 
+           div([
+            h1('Welcome, ' + state.lable ),
+            input({ sel: 'namecomp', type: 'text', value: state.value })
+          ])
+        );
+    const sinks = {
+        react: vdom$,
+        value: state$.map(state => state.value)
+    }
+    return sinks;
+  }
   
